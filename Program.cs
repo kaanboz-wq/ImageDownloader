dbb6c71f8ab8048744c554d96a8687d1913872a7
Kaan Boz <kaanboz@kuika.com>
2025-10-22 11:18:26 +0300

    lastest

--- Program.cs
+++ Program.cs
@@ -1,18 +1,13 @@
-using Microsoft.Extensions.Configuration;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
-app.UseHttpsRedirection();
 
 app.MapGet("/getimage", (String filePath = "") =>
 {
@@ -42,8 +37,6 @@ app.MapGet("/getimage", (String filePath = "") =>
     {
         return Results.BadRequest("Not Found.");
     }
-    
-
 }).WithName("GetImage");
 
 app.Run();
